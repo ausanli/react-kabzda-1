f9de36f6e8d2ed10cc9de433082f031b1d65ae92
anna <anna.bollbot@gmail.com>
2022-02-23 19:57:04 +0200

    setting local status for profile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import {connect} from "react-redux";
-import {getUserProfile} from "../../Redux/profile-reducer";
-import {Navigate, useParams} from "react-router-dom";
+import {getStatus, getUserProfile, updateStatus} from "../../Redux/profile-reducer";
+import { useParams} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
+
 
 
 
@@ -16,15 +16,16 @@ class ProfileContainer extends React.Component {
         if (this.props.params.profileId) {
             userId = this.props.params.profileId;
         }
-
         this.props.getUserProfile(userId);
+
+        this.props.getStatus(userId);
     }
 
     render() {
 
 
         return (
-            <Profile {...this.props} profile={this.props.profile}/>
+            <Profile {...this.props} profile={this.props.profile} status = {this.props.status} updateStatus ={this.props.updateStatus}/>
 
         )
     }
@@ -40,9 +41,10 @@ const withRouter = WrappedComponent => props => {
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
+    status: state.profilePage. status,
 });
 
-export default compose(connect(mapStateToProps, {getUserProfile}),
+export default compose(connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
     withAuthRedirect)(ProfileContainer);
 
