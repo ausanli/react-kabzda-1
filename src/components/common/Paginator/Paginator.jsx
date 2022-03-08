b5d7a59c85a665f416bf3359c79c50f7c1f22a8d
anna <anna.bollbot@gmail.com>
2022-03-08 14:55:00 +0200

    paginator refactoring

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,21 +1,37 @@
 import styles from "./Paginator.module.css";
-import React from "react";
+import React, {useState} from "react";
+import cn from "classnames";
 
+let Paginator = ({currentPage,totalItemsCount,pageSize,onPageChanged, portionSize=10}) => {
 
-let Paginator = ({currentPage,totalUsersCount,pageSize,onPageChanged}) => {
-
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    return <div>
-        {pages.map(p => {
-            return <span className={currentPage === p && styles.selectedPage} onClick={(event) => {
+
+    let portionCount = Math.ceil(pagesCount/portionSize);
+    let [portionNumber, setPortionNumber]=useState(1);
+    let leftPortionPageNumber=(portionNumber-1)*portionSize+1;
+    let rightPortionPageNumber=portionNumber*portionSize;
+
+    return <div className={styles.paginator}>
+        {portionNumber>1&&
+            <button onClick={()=>{setPortionNumber(portionNumber-1)}}>PREV</button>}
+
+        {pages
+            .filter(p=>p>=leftPortionPageNumber&&p<=rightPortionPageNumber)
+            .map((p )=> {
+            return <span className={cn({
+                [ styles.selectedPage]:currentPage===p
+            }, styles.pageNumber)}
+                         key={p}
+                onClick={(event) => {
                 onPageChanged(p)
             }}>{p} </span>;
         })}
-
+        {portionCount>portionNumber&&
+            <button onClick={()=>{setPortionNumber(portionNumber+1)}}> NEXT</button>}
 
     </div>
 }
---
+++ src/components/common/Paginator/Paginator.tests.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import Paginator from "./Paginator";
+
+describe("Paginator component tests", () => {
+    test("page count is 11 but should be shown only 10", () => {
+        const component = create(<Paginator totalItemsCount={11} pageSize={1} portionSize={10} />);
+        const root = component.root;
+        let spans = root.findAllByType("span");
+
+        expect(spans.length).toBe(1);
+    });
+    test("if pages count is more than 10 button NEXT should be present", () => {
+        const component = create(<Paginator totalItemsCount={11} pageSize={1} portionSize={10} />);
+        const root = component.root;
+        let button = root.findAllByType("button");
+
+        expect(button.length).toBe(1);
+    });
+});
